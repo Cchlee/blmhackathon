665f7969691f4fe4bf07ef9a78c3e6a145c51aaa
Lena Renshaw <lenarenshaw@Lenas-MacBook-Pro-2.local>
2020-06-12 20:47:16 -0400

    this does not work

---
+++ scripts.js
@@ -0,0 +1,32 @@
+const MongoClient = require('mongodb').MongoClient;
+
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+async function main(){
+    /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+     */
+    const uri = "mongodb+srv://blm:BlackLivesMatterHack@cluster0-8a3x0.mongodb.net/test?retryWrites=true&w=majority";
+    const client = new MongoClient(uri);
+
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+
+        // Make the appropriate DB calls
+        await  listDatabases(client);
+
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+main().catch(console.error);
